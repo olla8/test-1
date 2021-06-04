64ec3f14191c68642320da25ca43dd9eea0d7b03
agomulka <ola.gomulka@gmail.com>
2021-06-04 10:43:37 +0200

    second commit in branch

--- src/main/java/ClassInBranch.java
+++ src/main/java/ClassInBranch.java
@@ -1,5 +1,11 @@
 public class ClassInBranch {
 
+    private int number;
+
+    public ClassInBranch(int number) {
+        this.number = number;
+    }
+
     public static void main(String[] args) {
         System.out.println("kwik");
     }
