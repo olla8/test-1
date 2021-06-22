a41044b2794065c3cb46c8dff19aaf1a4ae7a0a1
agomulka <ola.gomulka@gmail.com>
2021-06-22 12:08:06 +0200

    class in branch

--- src/main/java/ClassInBranch.java
+++ src/main/java/ClassInBranch.java
@@ -6,6 +6,10 @@ public class ClassInBranch {
         this.number = number;
     }
 
+    public String kwik(){
+        return "kwik";
+    }
+
     public static void main(String[] args) {
         System.out.println("kwik");
     }
