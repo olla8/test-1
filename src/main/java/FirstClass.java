a3fa86afb0fb40940c3d33e1874dead0012e2a1f
agomulka <ola.gomulka@gmail.com>
2021-06-22 12:30:31 +0200

    first clas

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FirstClass {
-
+    // mhhh destination
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
